d42e64fba9aee7bfac0f24d672d65a73048bb99c
Artur Bruno <arturjuliao20@gmail.com>
2021-06-27 09:24:43 -0300

    feat: Navigating the bottom navigation bar
    Creating necessary fragments and associating them with the
    bottom
    navigation bar, changing the Toolbar title and fragment according
    to the
    chosen option.

--- app/src/main/java/com/posart/blocodenotas/MainActivity.kt
+++ app/src/main/java/com/posart/blocodenotas/MainActivity.kt
@@ -2,6 +2,13 @@ package com.posart.blocodenotas
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -9,5 +16,15 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         setSupportActionBar(findViewById(R.id.toolbar))
+
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottom_navigation_view)
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController: NavController = navHostFragment.navController
+        val appBarConfiguration = AppBarConfiguration(setOf(R.id.favoritesFragment, R.id.annotationFragment,
+            R.id.tasksFragment))
+
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        bottomNavigationView.setupWithNavController(navController)
     }
 }
