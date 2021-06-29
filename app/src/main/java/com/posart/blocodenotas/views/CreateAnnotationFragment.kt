15de6a88de4418e902bded930d118127493b651c
Artur Bruno <arturjuliao20@gmail.com>
2021-06-29 08:34:40 -0300

    feat: Creating a different menu for the CreateAnnotationFrament fragment
    Creating an "Up" navigation button and a menu with an item to save the
    note

--- app/src/main/java/com/posart/blocodenotas/views/CreateAnnotationFragment.kt
+++ app/src/main/java/com/posart/blocodenotas/views/CreateAnnotationFragment.kt
@@ -1,10 +1,12 @@
 package com.posart.blocodenotas.views
 
 import android.os.Bundle
+import android.view.*
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.posart.blocodenotas.R
+import com.posart.blocodenotas.databinding.ActivityMainBinding
 import com.posart.blocodenotas.databinding.FragmentCreateAnnotationBinding
 
 class CreateAnnotationFragment : Fragment() {
@@ -15,7 +17,29 @@ class CreateAnnotationFragment : Fragment() {
     ): View {
         val binding = FragmentCreateAnnotationBinding.inflate(layoutInflater)
 
+        setHasOptionsMenu(true)
+
+        val activityBinding =
+            DataBindingUtil.bind<ActivityMainBinding>(requireActivity().findViewById(R.id.root_activity_main))
+        activityBinding?.toolbar?.setNavigationIcon(R.drawable.ic_baseline_arrow_back_24)
+
+
+
         return binding.root
     }
 
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.save_button -> {
+                findNavController().navigateUp()
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        menu.clear()
+        inflater.inflate(R.menu.menu_create_annotation, menu)
+        super.onCreateOptionsMenu(menu, inflater)
+    }
 }
